a7c8484ced533800428f31eaf9d4e01d04e2d6fc
Eric <eric.m.lombardo@gmail.com>
2021-08-26 01:34:44 -0400

    Create Quote scaffold and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_180706) do
+ActiveRecord::Schema.define(version: 2021_08_26_053334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2021_08_24_180706) do
     t.string "content"
     t.boolean "complete"
     t.integer "goal_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.text "quote"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
